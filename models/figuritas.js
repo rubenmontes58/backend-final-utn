8e896c703920283a23041138bb5c567e6f0c0d88
Ruben Montes <rubenmontes58@gmail.com>
2022-10-10 11:48:26 -0300

    terminado

---
+++ models/figuritas.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const FiguritasSchema = mongoose.Schema({
+
+
+    
+
+    pais: {
+        type: String,
+        required: true,
+
+    },
+
+    numerofigu: {
+        type: Number,
+        required: true,
+
+    },
+
+    cantidad: {
+        type: Number,
+        required: true,
+
+    },
+
+    descripcion: {
+        type: String,
+        required: true,
+
+    },
+
+    contacto: {
+        type: Number,
+        required: true,
+
+    },
+  
+
+
+
+});
+
+module.exports = mongoose.model('Figuritas', FiguritasSchema);
+
